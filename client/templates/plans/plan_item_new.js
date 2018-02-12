27e3a700bbac8d3359e52c99aaeba0c59ade6c24
roylecj <roylecj@gmail.com>
2018-02-13 09:13:43 +1000

    fixed single line updates of completion.

--- client/templates/plans/plan_item_new.js
+++ client/templates/plans/plan_item_new.js
@@ -1,4 +1,57 @@
 Template.planItemNew.helpers({
+  hideActions: function() {
+    if (!Session.get("donePressed") && !Session.get("plannedPressed")) {
+      return true
+    } else {
+      if (Session.get("donePressed") === "" && Session.get("plannedPressed") === "") {
+        return true
+      } else {
+        return false
+      }
+
+    }
+  },
+  currentStatus: function() {
+    // Lookup the current status of the item and record it.
+
+    var orgId = MyOrganisation.findOne({userId: Meteor.userId(), activeFlag: true}).organisationId;
+
+    var qStatus = MyActions.findOne({activeFlag: true, actionId: this._id, organisationId: orgId});
+
+    if (!qStatus) {
+      return ""
+    } else {
+      if (qStatus.completeFlag) {
+        return "Complete"
+      } else {
+        if (qStatus.planFlag) {
+          return "Planned"
+        } else {
+          return "Outstanding"
+        }
+      }
+    }
+  },
+  currentStatusColour: function() {
+    var orgId = MyOrganisation.findOne({userId: Meteor.userId(), activeFlag: true}).organisationId;
+
+    var qStatus = MyActions.findOne({activeFlag: true, actionId: this._id, organisationId: orgId});
+
+    if (!qStatus) {
+      return ""
+    } else {
+      if (qStatus.completeFlag) {
+        return "label-success"
+      } else {
+        if (qStatus.planFlag) {
+          return "label-primary"
+        } else {
+          return "label-danger"
+        }
+      }
+    }
+
+  },
   isActive: function() {
     var orgId = MyOrganisation.findOne({userId: Meteor.userId(), activeFlag: true}).organisationId;
 
@@ -38,21 +91,21 @@ Template.planItemNew.helpers({
     }
   },
   isCompleteChecked: function() {
-    if (Session.get("completeItem") === this._id) {
+    if (Session.get("donePressed") === this._id) {
       return true
     } else {
       return false
     }
   },
   isPlannedChecked: function() {
-    if (Session.get("plannedItem") === this._id) {
+    if (Session.get("plannedPressed") === this._id) {
       return true
     } else {
       return false
     }
   },
   showNotes: function() {
-    if (Session.get("completeItem") === this._id || Session.get("plannedItem") === this._id) {
+    if (Session.get("donePressed") === this._id || Session.get("plannedPressed") === this._id) {
       return true
     } else {
       return false
@@ -73,27 +126,24 @@ Template.planItemNew.events({
     Session.set("donePressed", "");
     Session.set("plannedPressed", "");
   },
-  'click .btnComplete': function(e, t) {
-    Session.set("completeItem", this._id);
-    Session.set("plannedItem", "");
-//    Meteor.call('completeAction', this._id);
-  },
-  'click .btnPlan': function(e, t) {
-    Session.set("plannedItem", this._id);
-    Session.set("completeItem", "");
-  },
   'click .btnDoneSave': function(e, t) {
-
-    // Meteor.call("saveActionCompleted", this._id);
-
     Session.set("donePressed", "");
     Session.set("plannedPressed", "");
-    sAlert.success('Saved');
+
+    var orgId = MyOrganisation.findOne({userId: Meteor.userId(), activeFlag: true}).organisationId;
+
+    var action = MyActions.findOne({organisationId: orgId, actionId: this._id, activeFlag: true})
+
+    if (action) {
+      Meteor.call("saveActionCompleted", action._id);
+      sAlert.success('Saved');
+    } else {
+      sAlert.danger("Problem saving action");
+    }
   },
   'click .btnPlanCancel': function(e, t) {
     Session.set("donePressed", "");
     Session.set("plannedPressed", "");
-
   },
   'click .btnPlanSave': function(e, t) {
     Session.set("donePressed", "");
