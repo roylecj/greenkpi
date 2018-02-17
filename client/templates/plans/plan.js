0055503dc221fa3c24f490561746a5748a9d07ae
Unknown <roylecj@gmail.com>
2018-02-17 10:17:23 +1000

    Add information on why actions aren't displayed
    Show a warning to the user, when they haven't setup their actions yet.

--- client/templates/plans/plan.js
+++ client/templates/plans/plan.js
@@ -3,7 +3,20 @@ Template.plan.onCreated(function() {
   Meteor.call('addCoreActions');
 });
 
+Template.plan.onRendered(function() {
+  var orgId = MyOrganisation.findOne({userId: Meteor.userId(), activeFlag: true}).organisationId;
+  
+    if (MyCategoryUse.find({activeFlag: true, organisationId: orgId, "categoryDetails.coreCategory": false}).count() === 0) {
+      Session.set("showCategoryWarning", true)
+    } else {
+      Session.set("showCategoryWarning", false)
+    }
+});
+
 Template.plan.helpers({
+  showCategoryWarning: function() {
+    return Session.get("showCategoryWarning")
+  },
   showEnergy: function() {
     if (Session.get("showFilter") === "ENERGY" || Session.get("showFilter") === "") {
       return true
